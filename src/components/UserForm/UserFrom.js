c3f5ceb7f9397623929adf671b1e130113e0b8a1
OLfa <saidiolfa86@gmail.com>
2022-01-27 21:52:06 +0000

    Adding User And UserForm Files

---
+++ src/components/UserForm/UserFrom.js
@@ -0,0 +1,9 @@
+export default function UserFrom() {
+    return (
+        <div>
+            <span>Name</span>
+            <span>LastName</span>
+            <span>Age</span>
+        </div>
+    );
+};
