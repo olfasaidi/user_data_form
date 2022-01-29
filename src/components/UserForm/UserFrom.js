479963b7609893a8fe5c8ab0e8fa1048a8a8c71d
Fernando Toledo <fernandotoledo23@gmail.com>
2022-01-29 16:48:18 -0500

    Adding props to User Form Component

--- src/components/UserForm/UserFrom.js
+++ src/components/UserForm/UserFrom.js
@@ -1,9 +1,31 @@
-export default function UserFrom() {
-    return (
-        <div>
-            <span>Name</span>
-            <span>LastName</span>
-            <span>Age</span>
-        </div>
-    );
-};
+import './UserFrom.css';
+
+export default function UserFrom(props) {
+	return (
+		<div className='user-form'>
+			<div className='uf-field'>
+				<label>Name:</label>
+				<span>{props.name}</span>
+			</div>
+			<div className='uf-field'>
+				<label>Last Name:</label>
+				<span>{props.lastName}</span>
+			</div>
+			<div className='uf-field'>
+				<label>Country:</label>
+				<span>{props.country}</span>
+			</div>
+			{/* 
+				Add this new fields to the component:
+				// Email:
+				// User Name:
+				// Gender: (Radio buttons)
+				// Address:
+				// Date of birth:
+				// City:
+				// State:
+				// Zip code:
+			 */}
+		</div>
+	);
+}
