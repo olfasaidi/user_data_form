d78e9baa2dbc7617b8c1053bf30cf15244adc75c
Fernando Toledo <fernandotoledo23@gmail.com>
2022-02-07 17:42:13 -0500

    Updating User form with own styles

--- src/components/UserForm/UserFrom.js
+++ src/components/UserForm/UserFrom.js
@@ -3,50 +3,52 @@ import { Button, Form } from 'react-bootstrap';
 
 export default function UserFrom(props) {
     return (
-        <>
+        <div className='user-form-container'>
             <Form className='user-form'>
 
-                <Form.Group className="mb-3">
+                <Form.Group className="user-form-group mb-3">
 
-                    <Form.Label className='labelStyle' >Name</Form.Label>
+			<Form.Group className="user-form-field-3 mb-3">
+				<Form.Label className='user-form-label labelStyle' >Name</Form.Label>
 
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
+				<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
+			</Form.Group>
 
-                </Form.Group>
+			<Form.Group className="user-form-field-3 mb-3">
+				<Form.Label className='user-form-label labelStyle' >lastName</Form.Label>
 
-                <Form.Group className="mb-3">
+				<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
+			</Form.Group>
 
-                    <Form.Label className='labelStyle' >lastName</Form.Label>
+			<Form.Group className="user-form-field-3 mb-3">
 
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
+				<Form.Label className='user-form-label labelStyle' >DateOfBirth</Form.Label>
 
-                </Form.Group>
+				<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
 
-                <Form.Group className="mb-3">
-
-                    <Form.Label className='labelStyle' >Country</Form.Label>
-
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
-
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-
-                    <Form.Label className='labelStyle' >Email</Form.Label>
-
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
+			</Form.Group>
 
                 </Form.Group>
 
-                <Form.Group className="mb-3">
+		<Form.Group className="user-form-group mb-3">
+			<Form.Group className="user-form-field-2 mb-3">
 
-                    <Form.Label className='labelStyle' >Username</Form.Label>
+				<Form.Label className='user-form-label labelStyle' >Username</Form.Label>
 
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
+				<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
 
-                </Form.Group>
+			</Form.Group>
 
-                <Form.Group className="mb-3">
+			<Form.Group className="user-form-field-2 mb-3">
+
+				<Form.Label className='user-form-label labelStyle' >Email</Form.Label>
+
+				<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
+
+			</Form.Group>
+		</Form.Group>
+
+                <Form.Group className="user-form-field-1 mb-3">
 
                     <Form.Label className='labelStyle'> Gender</Form.Label>
                     <Form.Check name='gender' type="radio" label="male" inline />
@@ -54,18 +56,17 @@ export default function UserFrom(props) {
 
                 </Form.Group>
 
+		{/* ### Modify below here ### */}
+
+		<Form.Group className="mb-3">
+			<Form.Label className='user-form-label labelStyle' >Country</Form.Label>
+
+			<Form.Control className='user-form-input inputStyle' type="text" placeholder="Type Name" />
+		</Form.Group>
 
                 <Form.Group className="mb-3">
 
-                    <Form.Label className='labelStyle' >Adress</Form.Label>
-
-                    <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
-
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-
-                    <Form.Label className='labelStyle' >DateOfBirth</Form.Label>
+                    <Form.Label className='labelStyle' >Address</Form.Label>
 
                     <Form.Control className='inputStyle' type="text" placeholder="Type Name" />
 
@@ -97,6 +98,6 @@ export default function UserFrom(props) {
 
             </Form>
             <Button> Submit</Button>
-        </>
+        </div>
     );
 }
